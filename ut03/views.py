db148a1befaf6259cc650f1ef86101054e68e163
nereagonzalez12 <ngonsel2502@iesmartinezm.es>
2023-10-25 10:58:02 +0200

    form

--- ut03/views.py
+++ ut03/views.py
@@ -5,4 +5,4 @@ def welcome(request):
     return render(request, "ut03/index.html", {})
 
 def crea_tablero(request):
-    return render(request, "ut03/index.html", {})
+    return render(request, "ut03/crea_tablero.html", {})
