3e8a062b107118d78147b7245d273e74e1f12f41
nereagonzalez12 <ngonsel2502@iesmartinezm.es>
2023-10-31 14:52:57 +0100

    validar numero de minas

--- ut03/forms.py
+++ ut03/forms.py
@@ -1,5 +1,23 @@
+from mimetypes import init
+
 from django import forms
+from django.core.exceptions import ValidationError
+
 
 class CreaTableroForm(forms.Form):
-    filas=forms.IntegerField(label='Filas', min_value=1, max_value=20, required='false')
-    columnas=forms.IntegerField(label='Columnas', min_value=1, max_value=15, required='false')
+    filas = forms.IntegerField(label='Filas', min_value=1, max_value=20, required=False)
+    columnas = forms.IntegerField(label='Columnas', min_value=1, max_value=15, required=False)
+    minas = forms.IntegerField(label='Minas', min_value=1, max_value=20, required=False)
+    def clean_minas(self):
+        # Los datos se cogen del diccionario cleaned_data
+        cleaned_data = super().clean()
+        nMinas = cleaned_data.get("minas")
+        nFilas = cleaned_data.get("filas")
+        nColumnas = cleaned_data.get("columnas")
+
+        minasMax=int((nFilas+nColumnas)/2)
+
+        if nMinas > minasMax:
+            raise ValidationError("Has puesto demasiadas minas")
+
+        return nMinas
--- ut03/views.py
+++ ut03/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from .forms import CreaTableroForm
 # Create your views here.
@@ -6,16 +7,20 @@ def welcome(request):
 
 def crea_tablero_form(request):
     # Si se ha enviado el formulario
-    tablero_form = CreaTableroForm()
-    if request.method == 'GET':
-        tablero_form = CreaTableroForm(request.GET)
+    if request.method == 'POST':
+        tablero_form = CreaTableroForm(request.POST)
         # Ejecutar validación
         if tablero_form.is_valid():
             # Los datos se cogen del diccionario cleaned_data
             columnas = tablero_form.cleaned_data['columnas']
             filas = tablero_form.cleaned_data['filas']
+            minas = tablero_form.cleaned_data['minas']
             return render(request, "ut03/muestra_tablero.html",
-                          {'filas': filas, 'columnas': columnas,
-                           'rango_filas':range(filas), 'rango_columnas': range(columnas)})
-    # Si se pide la página por primera vez
-    return render(request, "ut03/crea_tablero.html", {'form': tablero_form})
+                          {'filas': filas, 'columnas': columnas, 'minas': minas,
+                           'rango_filas': range(filas), 'rango_columnas': range(columnas)})
+        else:
+            return render(request, "ut03/crea_tablero.html", {'form': tablero_form})
+    elif request.method == "GET":
+        # Si se pide la página por primera vez
+        tablero_form = CreaTableroForm()
+        return render(request, "ut03/crea_tablero.html", {'form': tablero_form})
